19f635a120b0b6b6f45d8f308c82bcd38b91246f
Geoff King <geoffrey_a_king@outlook.com>
2018-08-04 09:21:08 -0400

    Homework7

---
+++ config.py
@@ -0,0 +1,5 @@
+#Twitter API
+consumer_key = "jlcmTgTnqMfFitfiWCXAI4Bj4"
+consumer_secret =  "7BxPDnLIpTphpfNHXIFMB9gKmpJZ7lr4oFFY8KeRlbRFgVQVez"
+access_token =  "1072864105-rdVoAK5fKWm3auNFCtMq8ONno3F71dwDfmUOB7T"
+access_token_secret =  "fsjBnyXqoJW3b6wZO1ZmEvjtGiYwujX0HTtzxcQbpYGhA"
